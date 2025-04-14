ca2fd47c3a61ba4bc924e30b436115db3d4e6b29
JonathanCaporal <2230106@itace.edu.mx>
2025-04-14 15:55:58 -0500

    Archivos listos para El despliegue(error estilos)

--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js' // ← Verifica que esta ruta sea correcta
+            ],
             refresh: true,
         }),
     ],
-    server: {
-        host: '192.168.1.76',
-        hmr: {
-            host: '192.168.1.76'
-        },
-    },
-    build: {
-        // Habilitar minificación
-        minify: 'terser',
-        // Dividir el código en chunks más pequeños
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['@/vendor'],
-                }
-            }
-        },
-        // Comprimir assets
-        assetsInlineLimit: 4096,
-        chunkSizeWarningLimit: 1000,
-    }
 });
