f8989bea6f9ab0cb13e3c76c20d4b3bc7282dda3
JonathanCaporal <2230106@itace.edu.mx>
2025-04-20 19:07:08 -0500

    Init con migraciones

--- init.sh
+++ init.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+!/bin/bash
 
-# # Esperar a que la base de datos esté lista
-# echo "Esperando a que la base de datos esté disponible..."
-# until nc -z -v -w30 $DB_HOST $DB_PORT; do
-#   echo "Esperando conexión a la base de datos..."
-#   sleep 5
-# done
+# Esperar a que la base de datos esté lista
+echo "Esperando a que la base de datos esté disponible..."
+until nc -z -v -w30 $DB_HOST $DB_PORT; do
+  echo "Esperando conexión a la base de datos..."
+  sleep 5
+done
 
-# # Ejecutar migraciones
-# echo "Ejecutando migraciones..."
-# php artisan migrate --force
+# Ejecutar migraciones
+echo "Ejecutando migraciones..."
+php artisan migrate --force
 
-# # Verificar si hay errores en las migraciones
-# if [ $? -ne 0 ]; then
-#   echo "Error al ejecutar migraciones. Abortando."
-#   exit 1
-# fi
+# Verificar si hay errores en las migraciones
+if [ $? -ne 0 ]; then
+  echo "Error al ejecutar migraciones. Abortando."
+  exit 1
+fi
 
-# # Ejecutar seeders con la bandera --force
-# echo "Ejecutando seeders..."
-# php artisan db:seed --class=Permissions --force
-# php artisan db:seed --class=PermissionsAssignate --force
+# Ejecutar seeders con la bandera --force
+echo "Ejecutando seeders..."
+php artisan db:seed --class=Permissions --force
+php artisan db:seed --class=PermissionsAssignate --force
 
-# # Verificar si hay errores en los seeders
-# if [ $? -ne 0 ]; then
-#   echo "Error al ejecutar seeders. Abortando."
-#   exit 1
-# fi
+# Verificar si hay errores en los seeders
+if [ $? -ne 0 ]; then
+  echo "Error al ejecutar seeders. Abortando."
+  exit 1
+fi
 
 
 
