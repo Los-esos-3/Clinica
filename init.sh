d59d78ceb28ac56624597cfc5c4ebd84d45aa5fc
JonathanCaporal <2230106@itace.edu.mx>
2025-04-20 20:04:06 -0500

    init sin migraciones

--- init.sh
+++ init.sh
@@ -5,41 +5,41 @@ if [ "$RAILWAY_ENVIRONMENT" = "production" ]; then
     export APP_URL=https://expemed.up.railway.app
 fi
 
-# Esperar a que la base de datos esté lista
-echo "Esperando a que la base de datos esté disponible..."
-until nc -z -v -w30 $DB_HOST $DB_PORT; do
-  echo "Esperando conexión a la base de datos..."
-  sleep 5
-done
+# # Esperar a que la base de datos esté lista
+# echo "Esperando a que la base de datos esté disponible..."
+# until nc -z -v -w30 $DB_HOST $DB_PORT; do
+#   echo "Esperando conexión a la base de datos..."
+#   sleep 5
+# done
 
-# Verificar si hay migraciones pendientes
-echo "Verificando migraciones pendientes..."
-MIGRATIONS_PENDING=$(php artisan migrate:status | grep "No" | wc -l)
+# # Verificar si hay migraciones pendientes
+# echo "Verificando migraciones pendientes..."
+# MIGRATIONS_PENDING=$(php artisan migrate:status | grep "No" | wc -l)
 
-if [ "$MIGRATIONS_PENDING" -gt 0 ]; then
-    # Ejecutar migraciones solo si hay pendientes
-    echo "Ejecutando migraciones pendientes..."
-    php artisan migrate --force
+# if [ "$MIGRATIONS_PENDING" -gt 0 ]; then
+#     # Ejecutar migraciones solo si hay pendientes
+#     echo "Ejecutando migraciones pendientes..."
+#     php artisan migrate --force
     
-    # Verificar si hay errores en las migraciones
-    if [ $? -ne 0 ]; then
-        echo "Error al ejecutar migraciones. Abortando."
-        exit 1
-    fi
+#     # Verificar si hay errores en las migraciones
+#     if [ $? -ne 0 ]; then
+#         echo "Error al ejecutar migraciones. Abortando."
+#         exit 1
+#     fi
     
-    # Ejecutar seeders solo si se aplicaron migraciones
-    echo "Ejecutando seeders..."
-    php artisan db:seed --class=Permissions --force
-    php artisan db:seed --class=PermissionsAssignate --force
+#     # Ejecutar seeders solo si se aplicaron migraciones
+#     echo "Ejecutando seeders..."
+#     php artisan db:seed --class=Permissions --force
+#     php artisan db:seed --class=PermissionsAssignate --force
     
-    # Verificar si hay errores en los seeders
-    if [ $? -ne 0 ]; then
-        echo "Error al ejecutar seeders. Abortando."
-        exit 1
-    fi
-else
-    echo "No hay migraciones pendientes. Saltando ejecución."
-fi
+#     # Verificar si hay errores en los seeders
+#     if [ $? -ne 0 ]; then
+#         echo "Error al ejecutar seeders. Abortando."
+#         exit 1
+#     fi
+# else
+#     echo "No hay migraciones pendientes. Saltando ejecución."
+# fi
 
 # Iniciar el servidor Laravel
 echo "Iniciando servidor Laravel..."
