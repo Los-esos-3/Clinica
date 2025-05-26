840629f97b5537d8f04d2dff7fd81828e7d35f81
JonathanCaporal <2230106@itace.edu.mx>
2025-05-26 09:36:07 -0500

    init con migraciones forzadas

--- init.sh
+++ init.sh
@@ -14,34 +14,15 @@ until nc -z -v -w30 $DB_HOST $DB_PORT; do
   sleep 5
 done
 
-# Verificar si hay migraciones pendientes
-echo "Verificando migraciones pendientes..."
-MIGRATIONS_PENDING=$(php artisan migrate:status | grep "No" | wc -l)
-
-if [ "$MIGRATIONS_PENDING" -gt 0 ]; then
-    # Ejecutar migraciones solo si hay pendientes
-    echo "Ejecutando migraciones pendientes..."
+    echo "Iniciando migraciones:"
     php artisan migrate --force
-    
-    # Verificar si hay errores en las migraciones
-    if [ $? -ne 0 ]; then
-        echo "Error al ejecutar migraciones. Abortando."
-        exit 1
-    fi
-    
+
     # Ejecutar seeders solo si se aplicaron migraciones
-    echo "Ejecutando seeders..."
+    echo "Ejecutando seeders:"
     php artisan db:seed --class=Permissions --force
     php artisan db:seed --class=PermissionsAssignate --force
     
-    # Verificar si hay errores en los seeders
-    if [ $? -ne 0 ]; then
-        echo "Error al ejecutar seeders. Abortando."
-        exit 1
-    fi
-else
-    echo "No hay migraciones pendientes. Saltando ejecución."
-fi
+
 
 Iniciar el servidor Laravel
 echo "Iniciando servidor Laravel..."
